6e1987055718fc3ed27384b7dfc517997d08dce5
ScattyScarr <helen2773@gmail.com>
2021-10-04 12:49:55 +1100

    first commit

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -3,13 +3,13 @@
 </script>
 
 <svelte:head>
-	<title>About</title>
+	<title>About Us</title>
 </svelte:head>
 
 <div class="content">
 	<h1>About this app</h1>
-
-	<h3>We have a project</h3>
+<h2>fabulous</h2>
+	<h3>We have a project funded by the federal and state governments</h3>
 	<p>
 		Our aim is to <br />
 		Prepare<br />
