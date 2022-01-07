4d64a22357bfe0697991f6b5bad76b4a0be9f503
ScattyScarr <helen2773@gmail.com>
2022-01-07 13:49:33 +1100

    first commit

---
+++ src/routes/survey/index.svelte
@@ -0,0 +1,42 @@
+<p>Are there any static water supplies? 
+</p>
+<input                                            id="static_water"
+                                                type="text"
+                                                class="block border border-orange-700 w-full p-1 rounded mb-1"
+                                                name="static_water"
+                                                required="required"
+                                                placeholder="Static Water"
+                                                autocomplete="?????"
+                                    />
+
+<div>
+  <input type="checkbox" id="Tanks" name="Tanks"
+         checked>
+  <label for="Tanks">Tanks</label>
+</div>
+
+<div>
+  <input type="checkbox" id="Swimming pools”
+name="Swimming pools"
+checked>
+  <label for="Swimming pools">Swimming pools
+</label>
+</div>
+
+<div>
+  <input type="checkbox" id="Dam(s)“ name="Dam(s)"
+         checked>
+  <label for="Dam(s)">Dam(s)</label>
+</div>
+
+<div>
+  <input type="checkbox" id="Creek" name="Creek"
+checked>
+  <label for="Creek">Creek</label>
+</div>
+
+<div>
+  <input type="checkbox" id="None" name="None">
+checked>
+  <label for="None">None</label>
+</div>
